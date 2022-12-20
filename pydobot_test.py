0ef8ebf3b2e07968aaf5012b06ed82bbca69b255
JaisonST <jaisonmanu@gmail.com>
2022-12-20 09:42:04 +0400

    edited functions

--- pydobot_test.py
+++ pydobot_test.py
@@ -13,4 +13,5 @@ print(f'x:{x} y:{y} z:{z} j1:{j1} j2:{j2} j3:{j3} j4:{j4}')
 device.move_to(x + 20, y, z, r, wait=False)
 device.move_to(x, y, z, r, wait=True)  # we wait until this movement is done before continuing
 
+
 device.close()
