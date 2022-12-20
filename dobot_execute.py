44acb5b414c910ad5bc315bd686cba50bda55a52
JaisonST <jaisonmanu@gmail.com>
2022-12-20 21:29:23 +0400

    conveyor test

--- dobot_execute.py
+++ dobot_execute.py
@@ -1,3 +1,39 @@
-f = open("demofile2.txt", "a")
-f.write("Now the file has more content!")
-f.close()
+#input values max(3x3)
+#example input
+
+#[[1,1,1][0,0,0][2,2,2]]
+#0 - red
+#1 - green
+#2 - blue
+
+#block placement
+#222
+#000
+#111
+
+import RPi.GPIO as GPIO 
+import time
+from serial.tools import list_ports
+from dobot_extensions import Dobot 
+
+available_ports = list_ports.comports()
+port = available_ports[0].device 
+device = Dobot(port=port, verbose=True)
+ 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+ir = 17 
+
+GPIO.setup(ir, GPIO.IN)
+
+
+for i in range(150):
+	if not GPIO.input(ir):
+		device.conveyor_belt_distance(0, False)
+	else: 
+		device.conveyor_belt_distance(50, True)
+	time.sleep(0.05)
+
+device.close()
+
