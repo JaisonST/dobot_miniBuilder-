48d655acf7104d8304a938236ce0fa45b45eb9f4
JaisonST <jaisonmanu@gmail.com>
2022-12-21 13:09:44 +0400

    created motion class

---
+++ motion_class.py
@@ -0,0 +1,28 @@
+from dobot_extensions import Dobot 
+from serial.tools import list_ports
+
+
+class Motion():
+    UNIT_VAL = 25
+
+    def __init__(self):
+        available_ports = list_ports.comports()
+        print(f'available ports: {[x.device for x in available_ports]}')
+        port = available_ports[3].device
+
+        self.device = Dobot(port=port, verbose=True)
+        (self.home_x, self.home_y, self.home_z, self.home_r, j1, j2, j3, j4) = self.device.pose()
+        self.current_pos = {"x":0, "y": 0, "z":4}
+    
+
+    def go_home(self): 
+        self.device.move_to(self.home_x, self.home_y, self.home_z, self.home_r, wait=True)
+
+    def collect_block(self): 
+        self.device.move_to(self.home_x, self.home_y, self.home_z - self.UNIT_VAL, self.home_r, wait = True)
+        self.device.suck(True)
+        self.go_home()
+    
+    def disconnect(self):
+        self.device.close()
+    
